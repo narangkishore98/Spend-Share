2878067d1e9c4183a357d30bac89a030614da672
Kishore Narang <narangkishore98@gmail.com>
2020-04-25 17:41:08 -0400

    [Kishore] Added new Model Classes and Firebase configurations

---
+++ Spend Share/Models/Group.swift
@@ -0,0 +1,16 @@
+//
+//  Group.swift
+//  Spend Share
+//
+//  Created by Kishore Narang on 2020-04-25.
+//  Copyright © 2020 Kishore Narang. All rights reserved.
+//
+
+import Foundation
+class Group
+{
+    var contacts:[Contact] = []
+    var totalExpenses:Float = 0
+    var items:[Item] = []
+    
+}
